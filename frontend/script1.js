654fae034926379d51302a3c8159167b96b5b4f6
deniss1798 <99506681+deniss1798@users.noreply.github.com>
2023-04-02 22:55:55 +0300

    Update script1.js

--- frontend/script1.js
+++ frontend/script1.js
@@ -1,11 +1,41 @@
+
+
 $(document).ready(function() {
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
+  $('#match-form').submit(function(event) {
+    event.preventDefault(); // отменяем стандартное поведение формы
+    
+    // скрываем форму
+    $('#match-form').hide('slow', function() {
+      // показываем сообщение об успешной отправке
+      $('.success-message').show('slow');
+    });
   });
 });
+
+$(document).ready(function() {
+  $('#match-form').submit(function(event) {
+    event.preventDefault(); // отменяем стандартное поведение формы
+    
+    // скрываем форму
+    $('#match-form').hide('slow', function() {
+      // показываем сообщение об успешной отправке
+      $('.success-message').show('slow');
+      
+      // отправляем данные формы на сервер
+      $.ajax({
+        url: 'save_match.php',
+        type: 'post',
+        data: $('#match-form').serialize(),
+        success: function(response) {
+          console.log(response);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+});
+
+
+
